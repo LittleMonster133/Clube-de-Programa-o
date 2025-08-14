eabd8b51e8f722e3e914ae2015ee30b90aa73703
LittleMonster133 <matos.gabriel@loyola.g12.br>
2025-08-14 15:00:09 -0300

    Create Aula 1.py

---
+++ Aula 1.py
@@ -0,0 +1,21 @@
+print('Hello World')
+
+nome = 'gabriel'
+print('meu nome é ' + nome)
+
+idade= 12
+print('eu tenho ' + str(idade) + ' anos ')
+
+turma= 'D'
+print ('minha turma é ' + turma)
+print()
+print()
+
+num1 = 5
+num2 = 10
+num3 = 5
+num4 = 10
+
+
+resultado = (num1 + num2 + num3 + num4) / 4
+print(resultado)
